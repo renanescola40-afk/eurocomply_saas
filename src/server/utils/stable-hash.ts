8da328e01741f0d0d565f076df00034e791ad860
renanescola40-afk <renanescola40@gmail.com>
2026-06-15 19:05:54 +0100

    Fix stable hash type compatibility

--- src/server/utils/stable-hash.ts
+++ src/server/utils/stable-hash.ts
@@ -1,7 +1,7 @@
 import { createHash } from 'node:crypto';
 
-import { stableJsonStringify } from './stable-json';
+import { stableJsonStringify, type StableJsonValue } from './stable-json';
 
 export function stableSha256(value: unknown): string {
-  return createHash('sha256').update(stableJsonStringify(value)).digest('hex');
+  return createHash('sha256').update(stableJsonStringify(value as StableJsonValue)).digest('hex');
 }
