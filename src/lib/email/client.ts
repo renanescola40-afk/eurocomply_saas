f49c0c2d5ac92f0eda5c9c103a0b4cad00012885
renanescola40-afk <renanescola40@gmail.com>
2026-06-26 22:30:38 +0100

    Point email wrapper to sanitized sender

--- src/lib/email/client.ts
+++ src/lib/email/client.ts
@@ -1,4 +1,4 @@
 import 'server-only';
 
-export { redactEmailSecrets, sendEmail } from './sender';
-export type { SendEmailInput } from './sender';
+export { redactEmailSecrets, sendEmail } from './server-sender';
+export type { SendEmailInput } from './server-sender';
