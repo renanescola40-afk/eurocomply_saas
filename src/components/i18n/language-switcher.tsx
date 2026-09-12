2cd5df227139b02e19817a32d98cd4fd681424d5
renanescola40-afk <renanescola40@gmail.com>
2026-09-12 18:36:44 +0100

    security: mark locale persistence cookie Secure

--- src/components/i18n/language-switcher.tsx
+++ src/components/i18n/language-switcher.tsx
@@ -33,12 +33,12 @@ function withCurrentLocationState(path: string) {
 function persistLocale(locale: Locale) {
   try {
     localStorage.setItem(localeStorageKey, locale);
-    document.cookie = `NEXT_LOCALE=${locale};path=/;max-age=31536000;samesite=lax`;
+    document.cookie = `NEXT_LOCALE=${locale};path=/;max-age=31536000;samesite=lax;secure`;
   } catch {}
 }
 
 function persistLocaleScript(locale: Locale) {
-  return `try{localStorage.setItem('${localeStorageKey}','${locale}');document.cookie='NEXT_LOCALE=${locale};path=/;max-age=31536000;samesite=lax'}catch(e){}`;
+  return `try{localStorage.setItem('${localeStorageKey}','${locale}');document.cookie='NEXT_LOCALE=${locale};path=/;max-age=31536000;samesite=lax;secure'}catch(e){}`;
 }
 
 type LanguageSwitcherProps = {
