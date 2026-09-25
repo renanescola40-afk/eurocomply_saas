457083c86d9fce4da710e1f4d65067687ca97bd3
renanescola40-afk <renanescola40@gmail.com>
2026-09-25 09:30:54 +0100

    chore(sentry): use supported config import

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from 'next';
-import { withSentryConfig } from '@sentry/nextjs';
+import { withSentryConfig } from '@sentry/nextjs/config';
 import createNextIntlPlugin from 'next-intl/plugin';
 
 const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
